2d30c2c780fb416dbd1dd45230f5015b27ab51ff
PracticeEveryday <developerforus@gmail.com>
2024-09-14 14:27:28 +0900

    :sparkles: env module 생성
    Custom Env Service 생성하여 관리

---
+++ src/common/env/env.service.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { EnvService } from "./env.service";
+import { ConfigModule } from "@nestjs/config";
+import { EnvEnum } from "./env.enum";
+
+describe("EnvService", () => {
+  let configService: EnvService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: [".env.test"],
+        }),
+      ],
+      providers: [EnvService],
+    }).compile();
+
+    configService = module.get<EnvService>(EnvService);
+  });
+
+  it("should be defined", () => {
+    expect(configService).toBeDefined();
+  });
+
+  it("configService.getOrThrow .env.test 파일에 값이 있다면 일치한다.", () => {
+    const port = configService.getOrThrow(EnvEnum.PORT);
+
+    expect(port).toBe("3000");
+  });
+});
---
+++ src/common/env/env.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+import { EnvEnum } from "./env.enum";
+
+@Injectable()
+export class EnvService {
+  constructor(private readonly configService: ConfigService) {}
+
+  getOrThrow<T>(key: EnvEnum): T {
+    if (!this.configService.getOrThrow(EnvEnum[key])) {
+      throw new Error(".env 파일에 프로퍼티가 없습니다.");
+    }
+
+    return this.configService.getOrThrow(EnvEnum[key]);
+  }
+}
