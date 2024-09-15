157b68a9185610d8c603d047e7fb2b9cbb1afc6e
PracticeEveryday <developerforus@gmail.com>
2024-09-15 16:07:26 +0900

    :sparkles: db drizzle-orm migration file 생성 및 푸시 script 세팅

--- src/libs/db/drizzle/drizzle-config.ts
+++ src/libs/db/drizzle/drizzle-config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config(); // 환경 변수 로드
 
 export default defineConfig({
-  schema: "./schema.ts",
+  schema: "src/libs/db/drizzle/schema.ts",
   dialect: "mysql",
+  out: "src/libs/db/drizzle/migrate",
   migrations: {
+    table: "migration",
     prefix: "supabase",
+  },
+  dbCredentials: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    port: 3306,
   },
 });
