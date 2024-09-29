5c5192abd2d9bcec1a4cf3afbef22261134c43fb
PracticeEveryday <developerforus@gmail.com>
2024-09-29 22:31:37 +0900

    :memo: 테스트 Document 형식 통일
    행위 >> 성공 or 실패 >> 결과

--- src/apps/todo/test/integration/todo.service.int-spec.ts
+++ src/apps/todo/test/integration/todo.service.int-spec.ts
@@ -17,7 +17,7 @@ describe('TodoService Int', () => {
     });
 
     describe('createTodo()', () => {
-        it('todo 생성 >> 생성 성공 시 생성된 ID를 반환한다.', async () => {
+        it('todo 생성 >> 성공 >> 생성된 ID를 반환한다.', async () => {
             const todoId = await todoService.createTodo('title');
 
             expect(todoId).toBeGreaterThan(0); // 숫자이면서 0보다 큰 값 확인
@@ -27,7 +27,7 @@ describe('TodoService Int', () => {
     });
 
     describe('getTodoById()', () => {
-        it('id로 todo 조회 성공 >> 조회 성공 시 해당 ID의 todo를 반환한다.', async () => {
+        it('id로 todo 조회 >> 성공 >> 해당 ID의 todo를 반환한다.', async () => {
             const todo = await todoService.getTodoById(testTodoId);
 
             expect(todo.title).toBe('title');
@@ -36,7 +36,7 @@ describe('TodoService Int', () => {
     });
 
     describe('getTodoByTitle()', () => {
-        it('title로 todo 조회 성공 >> 조회 성공 시 title 이름과 일치하는 todo를 반환한다.', async () => {
+        it('title로 todo 조회 >> 성공 >> title 이름과 일치하는 todo를 반환한다.', async () => {
             const todo = await todoService.getTodoByTitle('title');
 
             expect(todo.title).toBe('title');
@@ -45,23 +45,23 @@ describe('TodoService Int', () => {
     });
 
     describe('executeTodo()', () => {
-        it('Todo 실행 처리 성공 >> 성공 시 업데이트 된 id를 반환한다.', async () => {
+        it('Todo 실행 처리 >> 성공 >> 업데이트 된 id를 반환한다.', async () => {
             const resultTodoId = await todoService.executeTodo(testTodoId);
 
             expect(resultTodoId).toBe(testTodoId);
         });
 
-        it('Todo 실행 처리 실패 >> 이미 실행 처리된 id의 경우 에러를 반환한다.', async () => {
+        it('Todo 실행 처리 >> 실패 >> 이미 실행 처리된 id의 경우 에러를 반환한다.', async () => {
             await expect(todoService.executeTodo(testTodoId)).rejects.toThrow();
         });
 
-        it('Todo 실행 처리 실패 >> todo가 존재하지 않는 id의 경우 에러를 반환한다.', async () => {
+        it('Todo 실행 처리 >> 실패 >> todo가 존재하지 않는 id의 경우 에러를 반환한다.', async () => {
             await expect(todoService.executeTodo(10000000)).rejects.toThrow();
         });
     });
 
     describe('deleteOneById()', () => {
-        it('Todo 삭제 처리 성공 >> 성공 시 삭제 된 id를 반환한다.', async () => {
+        it('Todo 삭제  >> 성공 >> 삭제 된 id를 반환한다.', async () => {
             const resultTodoId = await todoService.deleteTodoById(testTodoId);
 
             expect(resultTodoId).toBe(testTodoId);
